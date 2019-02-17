7fa8abcb920de5584d5d00fae06edf8d85546d5b
karmenyip <47105284+karmenyip@users.noreply.github.com>
2019-02-17 16:38:29 +0800

    Add files via upload

---
+++ Tissue_UpSet.js
@@ -0,0 +1,13 @@
+{
+	"file": "https://raw.githubusercontent.com/karmenyip/Tissue/master/Tissue_Upset_data_original.csv",
+	"name": "Tissues of MOR: small molecules for targeted metabolomics",
+	"header": 0,
+	"separator": ";",
+	"skip": 0,
+	"meta": [
+		{ "type": "id", "index": 0, "name": "Label" }
+	],
+	"sets": [
+		{ "format": "binary", "start": 1, "end": 14 }
+	]
+}
