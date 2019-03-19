6020a46ea6ddd2aa06ce062473c4d056cf92296f
karmenyip <47105284+karmenyip@users.noreply.github.com>
2019-03-19 16:56:05 +0800

    Add files via upload

---
+++ Table S5.js
@@ -0,0 +1,13 @@
+{
+	"file": "https://raw.githubusercontent.com/karmenyip/Tissue/master/13%20Table%20S5%20Occurrence%20of%20each%20feature%20in%2014%20groups%20of%20MOR%20samples%20for%20UpSet.csv",
+	"name": "Tissues of MOR: small molecules for targeted metabolomics",
+	"header": 0,
+	"separator": ";",
+	"skip": 0,
+	"meta": [
+		{ "type": "id", "index": 0, "name": "Label" }
+	],
+	"sets": [
+		{ "format": "binary", "start": 1, "end": 14 }
+	]
+}
